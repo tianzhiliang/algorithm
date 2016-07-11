5fe256e5afc9a08e7e132b27a3917833c582c1d8
tianzhiliang <tianzhiliang@baidu.com>
2016-07-11 19:56:08 +0800

    get_top_K_by_heap: opti interface and add max_sort

--- get_top_K_by_heap/get_top_K_by_heap.cpp
+++ get_top_K_by_heap/get_top_K_by_heap.cpp
@@ -9,30 +9,30 @@
 using namespace std;
 
 namespace Heap {
-    struct KeyValue {
-        int index;
-        float value;
-    };
-
-    bool keyvalue_min_cmp(const KeyValue a, const KeyValue b) {
-        return a.value > b.value;
+    bool min_cmp(const std::pair<int, float>& a, const std::pair<int, float>& b) {
+        return a.second > b.second;
     }
 
-    int get_top_K_by_heap(const std::vector<float>& candidates, std::vector<KeyValue>& results, 
-            const int results_size) {
+    bool max_cmp(const std::pair<int, float>& a, const std::pair<int, float>& b) {
+        return 1 - min_cmp(a, b);
+    }
+
+    int get_top_K_by_heap(const std::vector<float>& candidates, 
+            std::vector<std::pair<int, float> >& results, 
+            const int results_size, bool is_increasing) {
         // copy heah K to build initialize heap
         const int candidates_size = candidates.size();
         results.resize(results_size);
         for (int i = 0; i < results_size; i++) {
-            results[i].index = i;
-            results[i].value = candidates[i];
+            results[i].first = i;
+            results[i].second = candidates[i];
         }
 
         // build initialize heap using head K scores O(3 * K)
-        make_heap(results.begin(), results.end(), keyvalue_min_cmp);
+        make_heap(results.begin(), results.end(), min_cmp);
         fprintf(stderr, "make_heap done\n");
         for (int i = 0; i < results_size; i++) {
-            fprintf(stderr, "%d:%f\t", i, results[i].value);
+            fprintf(stderr, "%d:%f\t", i, results[i].second);
         }
         fprintf(stderr, "\n");
 
@@ -41,30 +41,33 @@ namespace Heap {
         // O ((n-K) * log(K))
         for (int i = results_size; i < candidates_size; i++) {
             // compare top-of-heap and input data
-            if (candidates[i] < results[0].value) {
+            if (candidates[i] < results[0].second) {
                 continue;
             }
-            fprintf(stderr, "candidates[i]:%f results[0]:%f\n", candidates[i], results[0].value);
+            fprintf(stderr, "candidates[i]:%f results[0]:%f\n", candidates[i], results[0].second);
 
             fprintf(stderr, "before pop_heap\n");
             for (int j = 0; j < results_size; j++) {
-                fprintf(stderr, "%d:%f\t", j, results[j].value);
+                fprintf(stderr, "%d:%f\t", j, results[j].second);
             }
             fprintf(stderr, "\n");
             
             // pop before push O(2 * log(K))
-            pop_heap(results.begin(), results.end(), keyvalue_min_cmp);
+            pop_heap(results.begin(), results.end(), min_cmp);
             results.pop_back();
 
             // push
-            KeyValue kv = {i, candidates[i]};
-            results.push_back(kv);
+            results.push_back(std::make_pair(i, candidates[i]));
 
             // adjust O(log(K))
-            push_heap(results.begin(), results.end(), keyvalue_min_cmp);
+            if (is_increasing) {
+                push_heap(results.begin(), results.end(), max_cmp);
+            } else {
+                push_heap(results.begin(), results.end(), min_cmp);
+            }
             fprintf(stderr, "push_heap done\n");
             for (int j = 0; j < results_size; j++) {
-                fprintf(stderr, "%d:%f\t", j, results[j].value);
+                fprintf(stderr, "%d:%f\t", j, results[j].second);
             }
             fprintf(stderr, "\n");
         }
@@ -73,12 +76,13 @@ namespace Heap {
 }
 
 int main(int argc, char** argv) {
-    if (2 != argc) {
-        fprintf(stderr, "Usage:%s K_of_top_k", argv[0]);
+    if (3 != argc) {
+        fprintf(stderr, "Usage:%s K_of_top_k is_increasing_order", argv[0]);
         return -1;
     }
     
     int K = atoi(argv[1]);
+    int is_increasing_order = atoi(argv[2]);
     
     vector<float> candidates;
     string input_str;
@@ -88,13 +92,13 @@ int main(int argc, char** argv) {
     }
     fprintf(stderr, "read data done, candidates size:%d K:%d\n", candidates.size(), K);
 
-    vector<Heap::KeyValue> results;
-    Heap::get_top_K_by_heap(candidates, results, K);
+    vector<std::pair<int, float> > results;
+    Heap::get_top_K_by_heap(candidates, results, K, is_increasing_order);
 
-    sort(results.begin(), results.end(), Heap::keyvalue_min_cmp);
+    sort(results.begin(), results.end(), Heap::min_cmp);
 
     for (int i = 0; i < K; i++) {
-        printf("%d:%d:%f\n", i, results[i].index, results[i].value);
+        printf("%d:%d:%f\n", i, results[i].first , results[i].second);
     }
     
     return 0;
--- get_top_K_by_heap/run.sh
+++ get_top_K_by_heap/run.sh
@@ -2,5 +2,7 @@ input=input.txt
 output=output.txt
 errlog=errlog
 K=30
+#is_increasing_order=1
+is_increasing_order=0
 
-cat $input | ./get_top_K_by_heap $K > $output 2> $errlog
+cat $input | ./get_top_K_by_heap $K $is_increasing_order > $output 2> $errlog
